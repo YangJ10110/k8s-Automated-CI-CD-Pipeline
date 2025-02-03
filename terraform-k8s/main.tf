f52d864f481708b3ea12c4b114404ff93af2b362
YangJ10110 <jeromeyang21@gmail.com>
2025-02-04 00:26:19 +0800

    working jenkins update

--- terraform-k8s/main.tf
+++ terraform-k8s/main.tf
@@ -11,6 +11,12 @@ resource "kubernetes_namespace" "prod" {
   }
 }
 
+resource "kubernetes_namespace" "jenkins" {
+  metadata {
+    name = "jenkins"
+  }
+}
+
 
 # backend - flask 
 
@@ -160,5 +166,104 @@ resource "kubernetes_service" "mongodb" {
     }
 
     type = "ClusterIP"
+  }
+}
+
+# jenkins using the devopsjourney1/jenkins-blueocean:2.332.3-1 image
+
+# deployment resource
+resource "kubernetes_deployment" "jenkins" {
+  metadata {
+    name      = "jenkins-resource"
+    namespace = kubernetes_namespace.jenkins.metadata[0].name
+  }
+
+  spec {
+    replicas = 1
+
+    selector {
+      match_labels = {
+        app = "jenkins"
+      }
+    }
+
+    template {
+      metadata {
+        labels = {
+          app = "jenkins"
+        }
+      }
+
+      spec {
+        container {
+          name             = "jenkins"
+          image            = "jenkins/jenkins:lts-jdk17" # Or a specific LTS version you prefer
+          image_pull_policy = "Always"
+          port {
+            container_port = 8080 # Jenkins default port
+          }
+          resources { # Important to set resource limits and requests
+            requests = {
+              cpu    = "500m" # Adjust as needed
+              memory = "1Gi"  # Adjust as needed
+            }
+            limits = {
+              cpu    = "1"    # 1 CPU core
+              memory = "2Gi"  # 2 GiB of memory
+            }
+          }
+           # Add a volume mount for persistence (Highly Recommended!)
+          volume_mount {
+            mount_path = "/var/jenkins_home"
+            name       = "jenkins-data" # Matches the volume name below
+          }
+        }
+        # Define the persistent volume
+        volume {
+          name = "jenkins-data"
+          persistent_volume_claim {
+            claim_name = kubernetes_persistent_volume_claim.jenkins.metadata[0].name
+          }
+        }
+
+      }
+    }
+  }
+}
+
+resource "kubernetes_service" "jenkins" {
+  metadata {
+    name      = "jenkins-service"
+    namespace = kubernetes_namespace.jenkins.metadata[0].name
+  }
+
+  spec {
+    selector = {
+      app = "jenkins"
+    }
+
+    port {
+      port        = 8080  # Service port (can be anything)
+      target_port = 8080    # Container port
+      node_port   = 31635 # Explicitly set the NodePort (important!)
+    }
+
+    type = "NodePort"
+  }
+}
+
+
+resource "kubernetes_persistent_volume_claim" "jenkins" {
+  metadata {
+    name      = "jenkins-pvc"
+    namespace = kubernetes_namespace.jenkins.metadata[0].name
+  }
+  spec {
+    access_modes = ["ReadWriteOnce"] # Or ReadWriteMany if your storage supports it
+    resources {
+      requests = {
+        storage = "10Gi" # Adjust as needed
+      }
+    }
   }
 }
